6163c3116dfa76f03ceb011d09b82239305a3ec0
JoshuaGruner <joshuagruner212@gmail.com>
2023-09-18 09:20:55 +0800

    Second commit

--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As String
+    End Sub
 End Class
